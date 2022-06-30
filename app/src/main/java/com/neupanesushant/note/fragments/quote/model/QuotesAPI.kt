7ba08c1ba74998c69aa7f0c941355dd5056f4c6c
Sushant Neupane <nsushant09@gmail.com>
2022-06-30 17:05:07 +0545

    Used coroutines along with retrofit to make network calls

--- app/src/main/java/com/neupanesushant/note/fragments/quote/model/QuotesAPI.kt
+++ app/src/main/java/com/neupanesushant/note/fragments/quote/model/QuotesAPI.kt
@@ -1,9 +1,21 @@
 package com.neupanesushant.note.fragments.quote.model
 
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+
+private val  BASE_URL= "https://favqs.com/api/"
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
 
 interface QuotesAPI {
     @GET("qotd/")
-    fun getData() : Call<QuoteDate>
+    suspend fun getData(): QuoteDate
+}
+
+object QuotesAPIService{
+    val retrofitService : QuotesAPI by lazy { retrofit.create(QuotesAPI::class.java) }
 }
