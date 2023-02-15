c4c41c6d50ccd336828406282889a4cd188d05b3
Sushant Neupane <nsushant09@gmail.com>
2023-02-15 16:16:21 +0545

    Database Changes

--- app/src/main/java/com/neupanesushant/note/model/NoteDatabase.kt
+++ app/src/main/java/com/neupanesushant/note/model/NoteDatabase.kt
@@ -8,24 +8,4 @@ import androidx.room.RoomDatabase
 @Database(entities = [NoteDetails::class], version = 1, exportSchema = false)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDetailsDao() : NoteDetailsDAO
-//
-//    companion object{
-//        @Volatile
-//        private var INSTANCE : NoteDatabase? = null
-//        fun getDatabase(context : Context) : NoteDatabase{
-//            val tempInstance = INSTANCE
-//            if(tempInstance!= null){
-//                return tempInstance
-//            }
-//            synchronized(this){
-//                val instance = Room.databaseBuilder(
-//                    context.applicationContext,
-//                    NoteDatabase::class.java,
-//                    "user_database"
-//                ).build()
-//                INSTANCE = instance
-//                return instance
-//            }
-//        }
-//    }
 }
