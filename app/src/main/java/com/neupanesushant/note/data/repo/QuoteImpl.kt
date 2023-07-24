3072176371ef3f4b01c07f856d55f6062c1a37bd
Sushant Neupane <nsushant09@gmail.com>
2023-07-24 21:35:12 +0545

    fixes : Removing test quotes

--- app/src/main/java/com/neupanesushant/note/data/repo/QuoteImpl.kt
+++ app/src/main/java/com/neupanesushant/note/data/repo/QuoteImpl.kt
@@ -1,7 +1,6 @@
 package com.neupanesushant.note.data.repo
 
 import com.neupanesushant.note.domain.model.Quote
-import com.neupanesushant.note.data.repo.QuotesAPI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -10,22 +9,30 @@ import kotlinx.coroutines.withContext
 
 class QuoteImpl(private val quotesAPI: QuotesAPI) {
 
-
     val quotes: MutableStateFlow<ArrayList<Quote>> = MutableStateFlow(arrayListOf())
 
     suspend fun getQuotes() {
         withContext(Dispatchers.IO) {
             val tempList = arrayListOf<Quote>()
-            val numQuotes = 10
+            val numQuotes = 15
 
-            val deferredQuotes = List(numQuotes) {
-                async { quotesAPI.getData().quote }
+            while (tempList.size < numQuotes) {
+                val remainingQuotes = numQuotes - tempList.size
+                val deferredQuotes = List(remainingQuotes) {
+                    async { quotesAPI.getData().quote }
+                }
+
+                tempList.addAll(deferredQuotes.awaitAll())
+
+                val filteredQuotes = tempList.filter { quote ->
+                    quote.body.length > 15
+                }
+
+                tempList.clear()
+                tempList.addAll(filteredQuotes.take(numQuotes))
             }
 
-            tempList.addAll(deferredQuotes.awaitAll())
-            quotes.emit(tempList)
+            quotes.emit(ArrayList(tempList))
         }
     }
-
-
 }
